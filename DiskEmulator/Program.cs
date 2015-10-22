fcaf2a7c3322d5e4284d6000e9fd834a852c4e1a
Travis Alpers <arbiter34@gmail.com>
2015-10-22 17:07:48 -0600

    Added avg, variance, and std deviation to results
    Added random data run

--- DiskEmulator/Program.cs
+++ DiskEmulator/Program.cs
@@ -12,6 +12,13 @@ namespace DiskEmulator
 
         static void Main(string[] args)
         {
+            SetDataRun();
+            RandomDataRun();
+
+        }
+
+        private static void SetDataRun()
+        {
             turnAroundTimes = new Dictionary<string, Dictionary<Seek, long>>();
 
             List<Seek> seeks = SeekFactory.SetData();
@@ -26,6 +33,16 @@ namespace DiskEmulator
 
             turnAroundTimes.Add("FCFS", manager.TurnAroundTimes);
 
+            long sum = turnAroundTimes["FCFS"].Sum(x => x.Value);
+
+            double avg = sum / turnAroundTimes["FCFS"].Count;
+
+            double variance = Variance(turnAroundTimes["FCFS"], avg);
+
+            double stdDev = Math.Sqrt(variance);
+
+            Console.WriteLine("Avg: {0,-4}  Variance: {1, -4}  StdDev: {2, -4}\n\n", avg, variance, stdDev);
+
             manager.Init();
 
             Console.WriteLine("---=========== SSTF ===========---\n");
@@ -35,6 +52,14 @@ namespace DiskEmulator
             Console.WriteLine("\nTotal: {0} ms\n", manager.Time);
 
             turnAroundTimes.Add("SSTF", manager.TurnAroundTimes);
+
+            avg = sum / turnAroundTimes["SSTF"].Count;
+
+            variance = Variance(turnAroundTimes["SSTF"], avg);
+
+            stdDev = Math.Sqrt(variance);
+
+            Console.WriteLine("Avg: {0,-4}  Variance: {1, -4}  StdDev: {2, -4}\n\n", avg, variance, stdDev);
 
             manager.Init();
 
@@ -46,6 +71,14 @@ namespace DiskEmulator
 
             turnAroundTimes.Add("LOOK", manager.TurnAroundTimes);
 
+            avg = sum / turnAroundTimes["LOOK"].Count;
+
+            variance = Variance(turnAroundTimes["LOOK"], avg);
+
+            stdDev = Math.Sqrt(variance);
+
+            Console.WriteLine("Avg: {0,-4}  Variance: {1, -4}  StdDev: {2, -4}\n\n", avg, variance, stdDev);
+
             manager.Init();
 
             Console.WriteLine("---=========== CLOOK ===========---\n");
@@ -56,6 +89,105 @@ namespace DiskEmulator
 
             turnAroundTimes.Add("CLOOK", manager.TurnAroundTimes);
 
+            avg = sum / turnAroundTimes["CLOOK"].Count;
+
+            variance = Variance(turnAroundTimes["CLOOK"], avg);
+
+            stdDev = Math.Sqrt(variance);
+
+            Console.WriteLine("Avg: {0,-4}  Variance: {1, -4}  StdDev: {2, -4}\n\n", avg, variance, stdDev);
+        }
+
+        private static void RandomDataRun()
+        {
+            turnAroundTimes = new Dictionary<string, Dictionary<Seek, long>>();
+
+            List<Seek> seeks = SeekFactory.RandomData(50);
+
+            DiskManager manager = new DiskManager(seeks);
+
+            Console.WriteLine("---=========== FCFS ===========---\n");
+
+            while (!manager.FCFS()) { }
+
+            Console.WriteLine("\nTotal: {0} ms\n", manager.Time);
+
+            turnAroundTimes.Add("FCFS", manager.TurnAroundTimes);
+
+            long sum = turnAroundTimes["FCFS"].Sum(x => x.Value);
+
+            double avg = sum / turnAroundTimes["FCFS"].Count;
+
+            double variance = Variance(turnAroundTimes["FCFS"], avg);
+
+            double stdDev = Math.Sqrt(variance);
+
+            Console.WriteLine("Avg: {0,-4}  Variance: {1, -4}  StdDev: {2, -4}\n\n", avg, variance, stdDev);
+
+            manager.Init();
+
+            Console.WriteLine("---=========== SSTF ===========---\n");
+
+            while (!manager.SSTF()) { }
+
+            Console.WriteLine("\nTotal: {0} ms\n", manager.Time);
+
+            turnAroundTimes.Add("SSTF", manager.TurnAroundTimes);
+
+            avg = sum / turnAroundTimes["SSTF"].Count;
+
+            variance = Variance(turnAroundTimes["SSTF"], avg);
+
+            stdDev = Math.Sqrt(variance);
+
+            Console.WriteLine("Avg: {0,-4}  Variance: {1, -4}  StdDev: {2, -4}\n\n", avg, variance, stdDev);
+
+            manager.Init();
+
+            Console.WriteLine("---=========== LOOK ===========---\n");
+
+            while (!manager.LOOK()) { }
+
+            Console.WriteLine("\nTotal: {0} ms\n", manager.Time);
+
+            turnAroundTimes.Add("LOOK", manager.TurnAroundTimes);
+
+            avg = sum / turnAroundTimes["LOOK"].Count;
+
+            variance = Variance(turnAroundTimes["LOOK"], avg);
+
+            stdDev = Math.Sqrt(variance);
+
+            Console.WriteLine("Avg: {0,-4}  Variance: {1, -4}  StdDev: {2, -4}\n\n", avg, variance, stdDev);
+
+            manager.Init();
+
+            Console.WriteLine("---=========== CLOOK ===========---\n");
+
+            while (!manager.CLOOK()) { }
+
+            Console.WriteLine("\nTotal: {0} ms\n", manager.Time);
+
+            turnAroundTimes.Add("CLOOK", manager.TurnAroundTimes);
+
+            avg = sum / turnAroundTimes["CLOOK"].Count;
+
+            variance = Variance(turnAroundTimes["CLOOK"], avg);
+
+            stdDev = Math.Sqrt(variance);
+
+            Console.WriteLine("Avg: {0,-4}  Variance: {1, -4}  StdDev: {2, -4}\n\n", avg, variance, stdDev);
+        }
+
+        private static double Variance(Dictionary<Seek, long> dict, double avg)
+        {
+            List<double> diff = new List<double>();
+
+            foreach (KeyValuePair<Seek, long> entry in dict)
+            {
+                diff.Add(Math.Pow(Math.Abs(entry.Value - avg), 2));
+            }
+            return diff.Average();
         }
     }
 }
